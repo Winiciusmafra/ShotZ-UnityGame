025d4bd8095af669f306be4c5e1f656003b1c4de
winiciusmafra <winicius.mafra@gmail.com>
2021-12-06 13:59:00 -0300

    camera follows the player and started adding the box colliders

--- ShotZ/Assets/ControlPlayer.cs
+++ ShotZ/Assets/ControlPlayer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ControlPlayer : MonoBehaviour
 {
-    public float Velocidade = 3;
+    public float Velocidade = 0.9f;
     // Start is called before the first frame update
     void Start()
     {
